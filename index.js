fd6ea1a60d10b2d114bf5985db595e26b1aee6cf
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-02 15:53:23 +0530

    adding start html

---
+++ index.js
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Free Lawn Makeover</title>
+    </head>
+    <body>
+
+        <h1>Free Lawn Makeover</h1>
+        <p>By Al Bladez</p>
+
+        <p>Work one day per week for free</p>
+        <p>Spent time, energy, skill, device, toos, vehicle, employees</p>
+        <p>More over control ego, talk to unknown strangers</p>
+
+        <iframe 
+        width="560"
+         height="315"
+          src="https://www.youtube.com/embed/7B1WFt3CA2U"
+           title="YouTube video player" 
+           frameborder="0" 
+           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+           allowfullscreen>
+            
+        </iframe>
+    </body>
+</html>
